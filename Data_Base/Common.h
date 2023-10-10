ebe3b63026670d02ce7596595c7a473065b2f05a
briascoleonel <leonelbriasco@mi.unc.edu.ar>
2023-10-10 17:47:45 -0300

    Se eliminan funciones innecesarias de las common Se implementa la base de datos de manera similar al main server del TP1

--- Data_Base/Common.h
+++ Data_Base/Common.h
@@ -14,17 +14,13 @@
 #include <stdatomic.h>
 #include <signal.h>
 #include <sys/time.h>
+#include <sqlite3.h>
 
-#include "Server_Unix.h"
+
 #include "Server_IPv4.h"
-#include "Server_IPv6.h"
-#include "File_Writing_Thread.h"
 #include "Verificar_Argumentos_Server.h"
-#include "Config_Socket_Unix.h"
 #include "Config_Socket_IPv4.h"
-#include "Config_Socket_IPv6.h"
 #include "Handler.h"
-#include "Contador.h"
 
 
 #define SA struct sockaddr
@@ -57,66 +53,17 @@ struct local_writer_arg_struct {
 struct IPv4_arg_struct {
     char IPV4_Server_Address[MAXLINE];
     short unsigned int IPV4_iport;
-    char IPv4_Write_File_Name[MAXLINE];
-    pthread_mutex_t *global_lock;
-    long unsigned int *total_bytes_recv_global;
-    long unsigned int *ult_bytes_recv_global;
+    sqlite3 **db;
     int max_clientes;
     int *salir;
 };
-
-struct UNIX_arg_struct {
-    char UNIX_File_Name[MAXLINE];
-    char UNIX_Write_File_Name[MAXLINE];
-    pthread_mutex_t *global_lock;
-    long unsigned int *total_bytes_recv_global;
-    long unsigned int *ult_bytes_recv_global;
-    int max_clientes;
-    int *salir;
-};
-
-
-
-struct IPv6_arg_struct {
-    char IPV6_Server_Address[MAXLINE];
-    short unsigned int IPV6_iport;
-    char IPV6_Interface[MAXLINE];
-    char IPv6_Write_File_Name[MAXLINE];
-    pthread_mutex_t *global_lock;
-    long unsigned int *total_bytes_recv_global;
-    long unsigned int *ult_bytes_recv_global;
-    int max_clientes;
-    int *salir;
-};
-
-enum Status {
-    Start = 1,
-    Expire = 2
-};
-
-
-struct Info_t{
-    atomic_int status;
-    char filename[MAXLINE];
-    FILE *destFile;
-    long unsigned int *TotalBytesRcv;
-    long unsigned int *LastBytesRcv;
-    long unsigned int bandwidth;
-    unsigned long int cont;
-    pthread_mutex_t *lock;
-    timer_t timerId;
-} ;
 
 int dir_IPv4_valida(char *ipAddr);
-int dir_IPv6_valida(char *ipAddr);
 void ocupar_handler(int *Handlers, int i, pthread_mutex_t *lock);
 void liberar_Handler(int *Handlers, int i, pthread_mutex_t *lock);
 int filename_valido(char *string); 
 int get_prim_hand_disp(int *Handlers, long unsigned int maxHandlers);
 int get_cant_hand_disp(int *Handlers, long unsigned int maxHandlers);
-void Hndlr(union sigval sigev_value);
-void TimerInit(struct Info_t* info);
-
 
 
 
--- Data_Base/Data_Base.c
+++ Data_Base/Data_Base.c
@@ -2,5 +2,40 @@
 
 int main(int argc, char *argv[])
 {
+    sqlite3 *db;
+    char *msg_err = 0;
+
+    pthread_t IPv4_Server_Thread;
+    struct IPv4_arg_struct IPv4_argumentos;
+
+    //Mecanismo de salida
+    char salir[MAXLINE];
+    int salir_todos = 0;
+
+    verificar_argumentos(argc,argv);
+
+    //Asignacion de argumentos de acuerdo a la estructura de cada hilo
     
+    //IPv4
+    strcpy(IPv4_argumentos.IPV4_Server_Address,argv[1]);
+    IPv4_argumentos.IPV4_iport = (short unsigned int)atoi(argv[2]);
+    IPv4_argumentos.db = &db;
+    IPv4_argumentos.salir = &salir_todos;
+
+    pthread_create(&IPv4_Server_Thread,NULL,Server_IPv4_codigo, &IPv4_argumentos);
+
+    //Mecanismo de salida
+    while (!salir_todos) 
+    {
+        printf("Ingrese -salir- para cerrar el server\n");
+        fgets(salir, sizeof(salir), stdin);
+        if (strcmp(salir, "salir\n") == 0) 
+        {
+            salir_todos = 1;
+        } 
+        else 
+        {
+            printf("No ha ingresado salir. Ha ingresado: %s\n", salir);
+        }
+    }
 }
