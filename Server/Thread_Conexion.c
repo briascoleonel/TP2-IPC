c369141dd36941d177f2ebad3fe67218cb9695c7
briascoleonel <leonelbriasco@mi.unc.edu.ar>
2023-12-01 10:08:30 -0300

    Se implementa la recepcion del ack Se obtiene el mensaje del tamaño y se redirecciona el archivo Luego se quita el request de la cola y se libera el handler

--- Server/Thread_Conexion.c
+++ Server/Thread_Conexion.c
@@ -204,46 +204,98 @@ void *Thread_Conex_Codigo(void *arg)
                     {
                         break;
                     }
-            }
+                }
 
-            memset(env_msg,0,MAXLINE);
-            strcat(env_msg,"Tam recibido\n");
-            cant_bytes_env = strlen(env_msg);
-            escr_ret_val = write(connfd,env_msg,cant_bytes_env);
-            if((escr_ret_val == -1) || ((long unsigned int)escr_ret_val != cant_bytes_env))
-            {
-                printf("Error al enviar mensaje\n");
-                //printf("El errno es: ")
-                exit(EXIT_FAILURE);
-            }
-            escr_ret_val = 0;
-            cant_bytes_env = strlen(recvline);
-            while(escr_ret_val <= 0)
-            {
-                if(*(arguments->salir) == 0)
+                memset(env_msg,0,MAXLINE);
+                strcat(env_msg,"Tam recibido\n");
+                cant_bytes_env = strlen(env_msg);
+                escr_ret_val = write(connfd,env_msg,cant_bytes_env);
+                if((escr_ret_val == -1) || ((long unsigned int)escr_ret_val != cant_bytes_env))
                 {
-                    escr_ret_val = send(*(req->conn), recvline,cant_bytes_env,MSG_DONTWAIT);
+                    printf("Error al enviar mensaje\n");
+                    //printf("El errno es: ")
+                    exit(EXIT_FAILURE);
+                }
+                escr_ret_val = 0;
+                cant_bytes_env = strlen(recvline);
+                while(escr_ret_val <= 0)
+                {
+                    if(*(arguments->salir) == 0)
+                    {
+                        escr_ret_val = send(*(req->conn), recvline,cant_bytes_env,MSG_DONTWAIT);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                cant_bytes_recv2 = 0;
+                memset(recvline2,0,MAXLINE);
+                while(cant_bytes_recv2 != 13)
+                {
+                    if(*(arguments->salir) == 0)
+                    {
+                        cant_bytes_recv2 = recv(*(req->conn),recvline2,MAXLINE-1,MSG_DONTWAIT);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                pthread_mutex_lock(arguments->ack_arg[arguments->id].ack_lock);
+                *(arguments->ack_arg[arguments->id].ack) = 0;
+                pthread_mutex_unlock(arguments->ack_arg[arguments->id].ack_lock);
+
+                char tam_file[MAXLINE];
+                strcpy(tam_file,recvline);
+                tam_file[strcspn(tam_file,"\n")] = 0;
+                long unsigned int Tam_File =(long unsigned int)atoi(tam_file);
+
+                long int rcv_bytes = 0;
+                long int bytes_escritos = 0;
+                while(Tam_File != 0)
+                {
+                    if(*(arguments->salir) == 0)
+                    {
+                        memset(recvline,0,MAXLINE);
+                        rcv_bytes = recv(connfd,recvline,MAXLINE-1,MSG_DONTWAIT);
+                        if(rcv_bytes > 0)
+                        {
+                            escr_ret_val = send(*(req->conn),recvline, (unsigned long int)rcv_bytes,MSG_DONTWAIT);
+                            if(escr_ret_val > 0)
+                            {
+                                bytes_escritos += escr_ret_val;
+                                Tam_File -= (unsigned long int)escr_ret_val;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if(close(*(req->conn)) < 0)
+                {
+                    printf("Soy conexion %d, Error al cerrar conexion %d\n",arguments->id,*(req->conn));
+                    //printf("El errno es: ")
                 }
                 else
                 {
-                    break;
+                    pthread_mutex_lock(arguments->list_lock);
+                    *(req->conn) = -1;
+                    pthread_mutex_unlock(arguments->list_lock);
                 }
-            }
-            cant_bytes_recv2 = 0;
-            memset(recvline2,0,MAXLINE);
-            while(cant_bytes_recv2 != 13)
-            {
-                if(*(arguments->salir) == 0)
-                {
-                    cant_bytes_recv2 = recv(*(req->conn),recvline2,MAXLINE-1,MSG_DONTWAIT);
-                }
-                else
-                {
-                    break;
-                }
+                pthread_mutex_lock(arguments->list_lock);
+                remove_req_list_head(arguments->list);
+                pthread_mutex_unlock(arguments->list_lock);
+                liberar_Handler(arguments->conx_disp, arguments->id, arguments->conx_lock);
             }
         }
+        else
+        {
+            break;
+        }
     }
-
-
+    return NULL;
 }
